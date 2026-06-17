73851194c1ebf9b80c398408b2a00b4656bb2265
Mico1104 <michealmarvellous782@gmail.com>
2026-06-17 23:41:06 +0100

    Add auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,117 @@
+import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default function Auth() {
+  const [mode, setMode] = useState("signup");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const { signup, user, logout, login } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function switchMode() {
+    if (mode === "signup") {
+      setMode("login");
+    } else {
+      setMode("signup");
+    }
+  }
+
+  function onSubmit(data) {
+    setError(null);
+    let result;
+    if (mode === "signup") {
+      result = signup(data.email, data.password);
+    } else {
+      result = login(data.email, data.password);
+    }
+
+    if (result.success) {
+      navigate("/");
+    } else {
+      setError(result.error);
+    }
+  }
   return (
-    <div>
+    <div className="page">
       {" "}
-      <h1>Auth Page</h1>
+      <div className="container">
+        <div className="auth-container">
+          {" "}
+          {user && <p>{user.email}</p>}
+          <button onClick={() => logout()}>Logout</button>
+          <h1 className="page-title">
+            {mode === "signup" ? "Sign up" : "Login"}
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="auth-form">
+            {error && <p className="error-message">{error}</p>}
+            <div className="form-group">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="form-input"
+                {...register("email", { required: "Email is required" })}
+              />
+              {errors.email && (
+                <span className="form-error">{errors.email.message}</span>
+              )}
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-input"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 4,
+                    message: "Password Characters must be at least 6",
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "Password Characters must be at most 12",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="form-error">{errors.password.message}</span>
+              )}
+            </div>
+            <button type="submit" className="btn btn-primary btn-large">
+              {mode === "signup" ? "Sign up" : "Login"}
+            </button>
+          </form>
+          <div className="auth-switch">
+            {mode === "signup" ? (
+              <p>
+                Already have an account?{" "}
+                <span className="auth-link" onClick={switchMode}>
+                  Login
+                </span>
+              </p>
+            ) : (
+              <p>
+                Don't have an account?{" "}
+                <span className="auth-link" onClick={switchMode}>
+                  Sign up
+                </span>
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
